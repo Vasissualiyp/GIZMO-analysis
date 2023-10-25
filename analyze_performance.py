57c20aff63a9802fc0f51bd90faee12b25e4e1f8
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2023-10-25 10:56:15 -0400

    Made the performance analysis more user-friendly, added comments

--- analyze_performance.py
+++ analyze_performance.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # or try 'TkAgg' if you want to display plots interactively
 import matplotlib.pyplot as plt
 
+# Set which runs you are interested in and which job_path you want to set for them in the final plot.
+# The path to runs can be changed in job_path_to_tables function
+labels_full = [
+               [ '2023.10.24:1', 'New compilation parameters'],
+               [ '2023.10.25:2', 'Old compilation parameters']
+               ]
+
+# Location of the output file
+output_file = '/cita/d/www/home/vpustovoit/plots/performance_analyzis.png'
+
+#--------------------SOURCE CODE------------------- {{{
 # Find branching points {{{
 def find_branching_point(main_data, branch_data, plot_type="redshift"):
     """
@@ -54,7 +67,7 @@ def plot_branch_diagram(dataframes, labels, output_file, plot_type="redshift"):
 
     Parameters:
     - dataframes: List of pandas DataFrames.
-    - labels: List of labels for each DataFrame.
+    - labels: List of job_path for each DataFrame.
     - plot_type: "redshift" or "scale_factor"
     """
     plt.figure(figsize=(12, 7))
@@ -88,8 +101,8 @@ def plot_branch_diagram(dataframes, labels, output_file, plot_type="redshift"):
     # plt.show()
 #}}}
 
-# Convert labels to tables, usable in the functions above {{{
-def labels_to_tables(labels):
+# Convert job_path to tables, usable in the functions above {{{
+def job_path_to_tables(labels):
     tables = []
     for label in labels:
         filename = '../output/'+label+'/performance_report.csv'
@@ -99,38 +112,11 @@ def labels_to_tables(labels):
         tables.append(table)
     return tables
 #}}}
+#--------------------MAIN------------------- }}}
 
-label1 = '2023.09.12:1'
-##label2 = '2023.09.11:2'
-##label3 = '2023.09.11:1'
-#label2 = '2023.09.12:13'
-label2 = '2023.09.13:7'
-label3 = '2023.09.13:9'
-#label3 = '2023.09.12:3'
-#label4 = '2023.09.12:4'
-#label5 = '2023.09.12:5'
-#label1 = '2023.09.12:6'
-#label2 = '2023.09.12:7'
-#label3 = '2023.09.12:8'
-#label4 = '2023.09.12:9'
-#label5 = '2023.09.12:10'
-#label6 = '2023.09.12:11'
-#label7 = '2023.09.12:12'
-#labels = [label1, label2, label3, label4, label5]
-#labels=[label1, label2, label3, label4, label5, label6, label7]
-labels=[label1, label2, label3]
+job_path = [item[0] for item in labels_full]
+plot_labels = [item[1] for item in labels_full]
 
-output_file = '/cita/d/www/home/vpustovoit/plots/performance_analyzis.png'
+tables = job_path_to_tables(job_path)
 
-tables = labels_to_tables(labels)
-#real_abels = ['OpenMP=2', label2, label3, label4, label5]
-real_labels = ['No Metals', 'With Metals, 198 MPI, OpenMP=2', 'With Metals, 96 MPI, OpenMP=4']
-#real_labels = [ 'OpenMP=2, 128 mpi processes', 
-#                'OpenMP=4, 64 mpi processes', 
-#                'OpenMP=4, 128 mpi processes', 
-#                'OpenMP=16, 128 mpi processes', 
-#                'OpenMP=16, 256 MPI processes',
-#                'OpenMP=1, 256 MPI processes',
-#                'OpenMP=8, 128 MPI processes']
-
-plot_branch_diagram(tables, real_labels, output_file, plot_type="scale_factor")
+plot_branch_diagram(tables, plot_labels, output_file, plot_type="scale_factor")
