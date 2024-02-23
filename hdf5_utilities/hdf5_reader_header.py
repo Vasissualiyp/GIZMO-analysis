ce7fd4c2f4856c9f65539c7bd4844060cfd83b71
Vasilii Pusovoit <vasily.pustovoit@mail.utoronto.ca>
2024-02-23 13:27:20 -0500

    Now the same code also outputs the mass of a type 1 particle

--- hdf5_utilities/hdf5_reader_header.py
+++ hdf5_utilities/hdf5_reader_header.py
@@ -1,31 +1,6 @@
-#import h5py {{{
-#
-## Open the initial conditions file
-#with h5py.File("zeldovich_ics.hdf5", "r") as f:
-#
-#    # Read the header information
-#    header = f["Header"]
-#    box_size = header.attrs["BoxSize"]
-#    num_particles = header.attrs["NumPart_Total"]
-#    redshift = header.attrs["Redshift"]
-#    Omega_m = header.attrs["Omega0"]
-#    Omega_L = header.attrs["OmegaLambda"]
-#    Hubble_param = header.attrs["HubbleParam"]
-#
-#    # Output the summary to a text file
-#    with open("zeldovich_ics_summary.txt", "w") as out_file:
-#        out_file.write(f"Box size: {box_size}\n")
-#        out_file.write(f"Number of particles: {num_particles}\n")
-#        out_file.write(f"Redshift: {redshift}\n")
-#        out_file.write(f"Omega_m: {Omega_m}\n")
-#        out_file.write(f"Omega_L: {Omega_L}\n")
-#        out_file.write(f"Hubble parameter: {Hubble_param}\n")
-#}}}
-
 import h5py
 import sys
 
-# Open the file
 def main(hdf5_file_location):
     print(f"Reading snapshot file {hdf5_file_location}...")
     with h5py.File(hdf5_file_location, "r") as f:
@@ -39,6 +14,10 @@ def main(hdf5_file_location):
         #omega_m = header.attrs["Omega0"]
         #omega_l = header.attrs["OmegaLambda"]
         hubble_param = header.attrs["HubbleParam"]
+        # Read the mass of type 1 particles
+        type_1_masses = f["PartType1/Masses"]
+        mass_of_type_1_particle_gizmo = type_1_masses[0] # GIZMO mass units: 10^10 Msun
+        mass_of_type_1_particle_msun = mass_of_type_1_particle_gizmo / 10**10 # Rockstar mass units: Msun
         # Output the summary to a text file
         output_file = "zeldovich_ics_summary.txt"
         with open(output_file, "w") as out_file:
@@ -51,6 +30,7 @@ def main(hdf5_file_location):
             out_file.write("Number of particles type 4 (stars): {}\n".format(num_particles[4]))
             out_file.write("Number of particles type 5 (black holes/sinks): {}\n".format(num_particles[5]))
             out_file.write(f"Redshift: {redshift}\n")
+            out_file.write(f"PartType1 Mass: {mass_of_type_1_particle_msun}\n")
             #out_file.write(f"Omega_m: {omega_m}\n")
             #out_file.write(f"Omega_L: {omega_l}\n")
             out_file.write(f"Hubble parameter: {hubble_param}\n")
